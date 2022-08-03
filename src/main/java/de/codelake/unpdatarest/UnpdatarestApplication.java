0de5238fca3eef4fcaec6ffc99798789e69c5159
Michael Kagel <michael.kagel@web.de>
2022-08-03 22:03:44 +0200

    Initial commit

---
+++ src/main/java/de/codelake/unpdatarest/UnpdatarestApplication.java
@@ -0,0 +1,13 @@
+package de.codelake.unpdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnpdatarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnpdatarestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/codelake/unpdatarest/UnpdatarestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.codelake.unpdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnpdatarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
